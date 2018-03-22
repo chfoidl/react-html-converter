823c82723d3896019072517306db2b297599cf35
Sethorax <info@sethorax.com>
2018-03-23 00:02:35 +0100

    Removed default export. Everything is a named export now.

--- src/integrations/browser.js
+++ src/integrations/browser.js
@@ -1,5 +1,5 @@
 import { BaseConverter, baseConvertStatic } from '../base-converter';
 import parser from '../parser/browser';
 
-export default () => BaseConverter(parser);
+export const ReactHTMLConverter = () => BaseConverter(parser);
 export const convertStatic = htmlString => baseConvertStatic(htmlString, parser);
